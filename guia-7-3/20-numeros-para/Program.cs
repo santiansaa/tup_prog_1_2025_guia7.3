c6246928c5fe4de37545b64acf210d9fb262b2a4
santiago ansa <santiagoansa4@gmail.com>
2025-06-06 14:51:48 -0300

    entrega actividad 1 guia 7-3

---
+++ guia-7-3/20-numeros-para/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_numeros_para
+{
+    class Program
+    {
+        static int num,acum,cont;
+        static double prom;
+
+        static void Main(string[] args)
+        {
+           
+            acum = 0;
+            for (int i = 0; i < 20; i++) 
+            {
+                
+                Console.WriteLine($"ingrese el numero {i + 1}");
+                num = Convert.ToInt32(Console.ReadLine());
+                acum = acum + num;
+            }
+            prom = acum / 20;
+            Console.WriteLine($"el promedio de los 20 numeros es {prom}");
+
+        }
+    }
+}
